5844dd8810b1dbbddf2255a196b8aae7d6d01d4d
Molly Thornton <mmthornto@gmail.com>
2019-11-07 20:40:02 +0000

    working

--- Creative4/public/javascripts/script.js
+++ Creative4/public/javascripts/script.js
@@ -1,27 +1,38 @@
-/*global axios*/
 let app = new Vue({
     el: "#app",
     data: {
-        quote: "",
-        author: "",
+        quote: '',
+        author: '',
         board: [],
     },
-    created:
-        function() {
+    created: function(){
         this.getQuotes();
     },
     methods: {
         async getQuotes() {
           try {
-              console.log("in function");
-              const response = await axios.get("/quotes");
+              let response = await axios.get("/quotes");
               this.board = response.data;
-              console.log(response);
+              console.log(this.board);
               return true;
           }
           catch(error) {
               console.log(error);
           }
         },
+        addItem(){
+            var url="http://hello.mmthornton.com:8080/quotes";
+            axios.post(url,{
+                quote: this.quote,
+                author: this.author,
+            })
+            .then(response=>{})
+            .catch(e=>{
+                console.log(e);
+            });
+            this.quote='';
+            this.author='';
+            this.getQuotes();
+        },
     }
-})
+});
